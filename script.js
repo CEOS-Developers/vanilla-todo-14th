47f36b281a6019873fae88396eeffd6d3df8b1d6
Bokdol11859 <2019147551@yonsei.ac.kr>
2021-09-21 01:03:29 +0900

    final design, deletion in finished list still needs to be implemented

--- script.js
+++ script.js
@@ -18,14 +18,18 @@ function addTask(task) {
   const li = document.createElement('li');
   const span = document.createElement('span');
   const deleteBut = document.createElement('img');
+
   deleteBut.src = './img/bin.png';
   deleteBut.style.cssText = 'width:1rem; height: 1rem; margin-right: 1rem;';
   deleteBut.setAttribute('align', 'right');
   deleteBut.addEventListener('click', deleteTask);
+
   span.innerHTML = task.value;
   span.addEventListener('click', finishTask);
+
   li.append(span);
   li.append(deleteBut);
+
   todoList.appendChild(li);
 }
 
