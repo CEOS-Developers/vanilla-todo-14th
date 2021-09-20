a2ebe6feb369a620280d7a5cec75c71aa7a88a32
Bokdol11859 <2019147551@yonsei.ac.kr>
2021-09-20 23:45:31 +0900

    delete function implemented

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const todoForm = document.querySelector('.inputForm');
 const todoInput = todoForm.querySelector('input');
 const todoList = document.querySelector('.toDos');
+const doneList = document.querySelector('.dones');
 
 todoForm.addEventListener('submit', function (e) {
   e.preventDefault();
@@ -13,13 +14,23 @@ todoForm.addEventListener('submit', function (e) {
   todoForm.reset();
 });
 
-// deleteCurrent.addEventListener('click', deleteTask);
-
 function addTask(task) {
-  let li = document.createElement('li');
-
-  li.innerHTML = `${task} <span class = "delete"><img src="./img/bin.png" style="width:1rem; height: 1rem; margin-right: 1rem;" align = "right"></span>`;
+  const li = document.createElement('li');
+  const span = document.createElement('span');
+  const finishedBut = document.createElement('input');
+  const deleteBut = document.createElement('img');
+  deleteBut.src = './img/bin.png';
+  deleteBut.style.cssText = 'width:1rem; height: 1rem; margin-right: 1rem;';
+  deleteBut.setAttribute('align', 'right');
+  deleteBut.addEventListener('click', deleteTask);
+  span.innerHTML = task;
+  li.append(span);
+  li.append(deleteBut);
   todoList.appendChild(li);
 }
 
-// function deleteTask(task) {}
+function deleteTask(e) {
+  const button = e.target;
+  const li = button.parentNode;
+  todoList.removeChild(li);
+}
