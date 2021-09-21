50922ab26358b9efc0261e782255695922d11938
TaeeunKim <xodms0309@naver.com>
2021-09-21 22:32:38 +0900

    add: count todo

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ const addTodoButton=document.getElementById("todo-button");
 const doingTodoList=document.getElementById("doing");
 const doneTodoList=document.getElementById("done");
 let todoArray=[];
+const doingCount=document.getElementById("doing-count");
+const doneCount=document.getElementById("done-count");
 
 function createTodoList(todoObj){
     const li=document.createElement("li");
@@ -53,12 +55,19 @@ function deleteTodo(e){
 }
 
 /*save todo*/
-function saveTodo(){
-    localStorage.setItem("todo", JSON.stringify(todoArray));
-}
 const savedTodos=localStorage.getItem("todo");
 if (savedTodos){
     const parsedTodos=JSON.parse(savedTodos); //string을 array로 parse시켜줌
     todoArray=parsedTodos; //empty array가 아닌 이전 값을 모두 가지고 있는 array
     parsedTodos.forEach(todo=>createTodoList(todo));
 }
+function saveTodo(){
+    localStorage.setItem("todo", JSON.stringify(todoArray));
+    doingCount.innerText=todoArray.filter(todo=>todo.type===true).length;
+    doneCount.innerText=todoArray.filter(todo=>todo.type===false).length;
+}
+
+//새로고침 했을 때 count 초기화 되는거 방지
+window.onload = () => {
+    saveTodo();
+};
