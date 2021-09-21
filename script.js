c315db6728fd1e8b09be89bb7b953b2345e60f06
TaeeunKim <xodms0309@naver.com>
2021-09-21 18:55:21 +0900

    feat: save todo at localstorage

--- script.js
+++ script.js
@@ -2,21 +2,41 @@
 const todoInput=document.getElementById("todo-input");
 const addTodoButton=document.getElementById("todo-button");
 const doingTodoList=document.getElementById("doing");
+let todoArray=[];
 
-function createListItem(todo){
+function createTodoList(todoObj){
     const li=document.createElement("li");
     const img=document.createElement("img");
-    li.innerText=todo;
+    li.innerText=todoObj.text;
+    li.id=todoObj.id;
     img.src="./img/bin.png";
     li.appendChild(img);
     doingTodoList.appendChild(li);
 }
-function addTodo(e){
+function onClickAddTodo(e){
     e.preventDefault();
-    createListItem(todoInput.value);
+    const todoObj={
+        "text":todoInput.value,
+        "id":Date.now(),
+        "type": 1,
+    }
+    createTodoList(todoObj);
+    todoArray.push(todoObj);
+    saveTodo();
     todoInput.value="";
 }
-addTodoButton.addEventListener("click", addTodo);
+addTodoButton.addEventListener("click", onClickAddTodo);
 /*doing->done*/
 
 /*delete todo*/
+
+/*save todo*/
+function saveTodo(){
+    localStorage.setItem("todo", JSON.stringify(todoArray))
+}
+const savedTodos=localStorage.getItem("todo");
+if (savedTodos){
+    const parsedTodos=JSON.parse(savedTodos); //string을 array로 parse시켜줌
+    todoArray=parsedTodos; //empty array가 아닌 이전 값을 모두 가지고 있는 array
+    parsedTodos.forEach(todo=>createTodoList(todo));
+}
