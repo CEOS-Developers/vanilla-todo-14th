b72e8ec6a0e1a9703cbefe2ed17fdbee78174436
[Github <[Github>
2021-09-25 01:09:49 +0900

    update remove function

--- script.js
+++ script.js
@@ -1,12 +1,24 @@
 var submit = document.getElementById('submit');
 var toDoList = document.getElementById('toDoList');
 var unDoneList = document.getElementById('unDoneList');
+var count = 0;
 
 submit.addEventListener('click', clickButton);
 
+/*add to unDoneList*/
 function clickButton(){
   var temp = document.createElement('li');
-  
+  temp.setAttribute("class", "unDoneListItem");
+  temp.setAttribute("id", "li"+count);
   temp.innerHTML = toDoList.value;
+  temp.innerHTML += "<button style = 'float: right;' type='button' onclick='remove("+count+")'>삭제</button>";
   unDoneList.appendChild(temp);
+  count++;
 }
+
+
+/* remove function */
+function remove(count) {
+    var li = document.getElementById('li'+count);
+    unDoneList.removeChild(li);
+  }
