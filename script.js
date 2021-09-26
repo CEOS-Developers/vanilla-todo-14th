2820041ef530fc2fdbb184f7f61eb85fa1417110
TaeeunKim <xodms0309@naver.com>
2021-09-26 17:00:21 +0900

    fix: set prettier

--- script.js
+++ script.js
@@ -1,79 +1,81 @@
 /* add todo*/
-const todoInput=document.getElementById("todo-input");
-const addTodoButton=document.getElementById("todo-button");
-const doingTodoList=document.getElementById("doing");
-const doneTodoList=document.getElementById("done");
-let todoArray=[];
-const doingCount=document.getElementById("doing-count");
-const doneCount=document.getElementById("done-count");
+const todoInput = document.getElementById('todo-input');
+const addTodoButton = document.getElementById('todo-button');
+const doingTodoList = document.getElementById('doing');
+const doneTodoList = document.getElementById('done');
+let todoArray = [];
+const doingCount = document.getElementById('doing-count');
+const doneCount = document.getElementById('done-count');
 
 /*create todo li*/
-function createTodoList(todoObj){
-    const todoListItem=document.createElement("li");
-    const deleteImg=document.createElement("img");
-    todoListItem.innerText="🐈 "+todoObj.text;
-    todoListItem.id=todoObj.id;
-    todoListItem.addEventListener("click", toggleTodo);
-    deleteImg.src="./img/bin.png";
-    deleteImg.addEventListener("click", deleteTodo);
-    todoListItem.appendChild(deleteImg);
-    todoObj.type? doingTodoList.appendChild(todoListItem):doneTodoList.appendChild(todoListItem);
+function createTodoList(todoObj) {
+  const todoListItem = document.createElement('li');
+  const deleteImg = document.createElement('img');
+  todoListItem.innerText = '🐈 ' + todoObj.text;
+  todoListItem.id = todoObj.id;
+  todoListItem.addEventListener('click', toggleTodo);
+  deleteImg.src = './img/bin.png';
+  deleteImg.addEventListener('click', deleteTodo);
+  todoListItem.appendChild(deleteImg);
+  todoObj.type
+    ? doingTodoList.appendChild(todoListItem)
+    : doneTodoList.appendChild(todoListItem);
 }
 
 /*click add todo button*/
-function onClickAddTodo(e){
-    e.preventDefault();
-    if (todoInput.value){
-        const todoObj={
-        "text":todoInput.value,
-        "id":Date.now(),
-        "type": true,
-        }
-        createTodoList(todoObj);
-        todoArray.push(todoObj);
-        saveTodo();
-    }
-    todoInput.value="";
+function onClickAddTodo(e) {
+  e.preventDefault();
+  if (todoInput.value) {
+    const todoObj = {
+      text: todoInput.value,
+      id: Date.now(),
+      type: true,
+    };
+    createTodoList(todoObj);
+    todoArray.push(todoObj);
+    saveTodo();
+  }
+  todoInput.value = '';
 }
-addTodoButton.addEventListener("click", onClickAddTodo);
+addTodoButton.addEventListener('click', onClickAddTodo);
 
 /*doing->done, done->doing*/
-function toggleTodo(e){
-    todoArray.forEach(todo=>{
-        if (todo.id===parseInt(e.target.id)){
-            const li=e.target; 
-            li.remove();
-            //type을 반전
-            todo.type=!todo.type;
-            createTodoList(todo);
-            saveTodo();
-        }
-    })
+function toggleTodo(e) {
+  todoArray.forEach((todo) => {
+    if (todo.id === parseInt(e.target.id)) {
+      const li = e.target;
+      li.remove();
+      //type을 반전
+      todo.type = !todo.type;
+      createTodoList(todo);
+      saveTodo();
+    }
+  });
 }
 
 /*delete todo*/
-function deleteTodo(e){
-    console.log(e);
-    const li=e.target.parentElement; 
-    li.remove();
-    todoArray=todoArray.filter(todo=>todo.id!=parseInt(li.id));
-    saveTodo();
+function deleteTodo(e) {
+  console.log(e);
+  const li = e.target.parentElement;
+  li.remove();
+  todoArray = todoArray.filter((todo) => todo.id != parseInt(li.id));
+  saveTodo();
 }
 
 /*save todo*/
-function saveTodo(){
-    localStorage.setItem("todo", JSON.stringify(todoArray));
-    doingCount.innerText=todoArray.filter(todo=>todo.type===true).length;
-    doneCount.innerText=todoArray.filter(todo=>todo.type===false).length;
+function saveTodo() {
+  localStorage.setItem('todo', JSON.stringify(todoArray));
+  doingCount.innerText = todoArray.filter((todo) => todo.type === true).length;
+  doneCount.innerText = todoArray.filter((todo) => todo.type === false).length;
 }
 
 //새로고침 했을 때 count 초기화 되는거 방지
 window.onload = () => {
-    const savedTodos=localStorage.getItem("todo");
-    if (savedTodos){
-        const parsedTodos=JSON.parse(savedTodos); //string을 array로 parse시켜줌
-        todoArray=parsedTodos; //empty array가 아닌 이전 값을 모두 가지고 있는 array
-        parsedTodos.forEach(todo=>createTodoList(todo));
-    }
-    saveTodo();
+  const savedTodos = localStorage.getItem('todo');
+  if (savedTodos) {
+    const parsedTodos = JSON.parse(savedTodos); //string을 array로 parse시켜줌
+    todoArray = parsedTodos; //empty array가 아닌 이전 값을 모두 가지고 있는 array
+    parsedTodos.forEach((todo) => createTodoList(todo));
+  }
+  saveTodo();
 };
